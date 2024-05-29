c52d0ebb3e0391960b2a93f4341f58f9275c0963
jacristi <jacristi0428@gmail.com>
2024-05-28 21:55:24 -0400

    initial commit

---
+++ components/move_component.gd
@@ -0,0 +1,9 @@
+class_name MoveComponent
+extends Node
+
+
+@export var velocity: Vector2
+@export var actor: Node2D
+
+func _process(delta: float) -> void:
+	actor.translate(delta * velocity)
---
+++ components/player_ship1_stats.gd
@@ -0,0 +1,5 @@
+class_name ShipStats
+extends Resource
+
+@export var v_move_speed: float = 50
+@export var h_move_speed: float = 75
