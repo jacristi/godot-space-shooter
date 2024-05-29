397113a7291db9ae1691b59707e4b76a4ebd14e4
jacristi <jacristi0428@gmail.com>
2024-05-29 13:55:07 -0400

    feat: enemy adds score

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var scale_component: ScaleComponent = $ScaleComponent
 @onready var flash_component: FlashComponent = $FlashComponent
 @onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var score_component: ScoreComponent = $ScoreComponent
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 @onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
 @onready var hitbox_component:  HitboxComponent  = $HitboxComponent
@@ -12,6 +13,7 @@ extends Node2D
 
 
 func _ready() -> void:
+    stats_component.no_health.connect(give_score_on_death)
     visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
     hurtbox_component.hurt.connect(was_hit)
     hitbox_component.hit_hurtbox.connect(destroyed_component.destroy.unbind(1))
@@ -21,4 +23,5 @@ func was_hit(hitbox: HitboxComponent) -> void:
     scale_component.tween_scale()
     shake_component.tween_shake()
     
-
+func give_score_on_death() -> void:
+    score_component.adjust_score()
--- scenes/game.gd
+++ scenes/game.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
-
+@export var game_stats: GameStats
 @onready var player_ship: Node2D = $PlayerShip
+@onready var score_label: Label = $ScoreLabel
+
 
 func _ready() -> void:
+    update_score_label(game_stats.score)
+    game_stats.score_changed.connect(update_score_label)
     player_ship.tree_exiting.connect(player_defeated)
 
 func player_defeated() -> void:
     await get_tree().create_timer(2.0).timeout
     get_tree().change_scene_to_file("res://ui/game_over.tscn")
+
+
+func update_score_label(new_score: int) -> void:
+    score_label.text = "Score: %d" % [new_score]
