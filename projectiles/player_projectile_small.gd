3a051f6609e79f5a9501ad3ce839abda1c28b208
jacristi <jacristi0428@gmail.com>
2024-05-28 22:52:48 -0400

    feat: improved animations

--- projectiles/player_projectile_small.gd
+++ projectiles/player_projectile_small.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var scale_component: ScaleComponent = $ScaleComponent
 
 
 func _ready() -> void:
+    scale_component.tween_scale()
     visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-
--- scenes/player/player_ship.gd
+++ scenes/player/player_ship.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var projectile_source_left: Marker2D    = $ProjectileSourceLeft
 @onready var projectile_source_right: Marker2D   = $ProjectileSourceRight
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var move_component: MoveComponent       = $MoveComponent
+@onready var ship_animated_sprite: AnimatedSprite2D   = $SpriteAnchor/ShipAnimatedSprite
+@onready var thrust_animated_sprite: AnimatedSprite2D = $SpriteAnchor/ThrustAnimatedSprite
 
 
 func _ready():
@@ -12,8 +15,23 @@ func _ready():
     pass # Replace with function body.
 
 
+func _process(delta: float) -> void:
+    animate_ship()
+    
+
 func fire_projectiles() -> void:
     spawner_component.spawn(projectile_source_left.global_position)
     spawner_component.spawn(projectile_source_right.global_position)
     scale_component.tween_scale()
     
+    
+func animate_ship() -> void:
+    if move_component.velocity.x < 0:
+        ship_animated_sprite.play("bank_left")
+        thrust_animated_sprite.play("bank_left")
+    elif move_component.velocity.x > 0:
+        ship_animated_sprite.play("bank_right")
+        thrust_animated_sprite.play("bank_right")
+    else:
+        ship_animated_sprite.play("center")
+        thrust_animated_sprite.play("center")
