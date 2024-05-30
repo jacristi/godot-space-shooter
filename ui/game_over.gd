12b3d4c1379ab34f5dc1b15ae1dad44d9a085b58
jacristi <jacristi0428@gmail.com>
2024-05-29 23:16:47 -0400

    feat: simple high_score save/load

--- ui/game_over.gd
+++ ui/game_over.gd
@@ -1,14 +1,21 @@
 extends Control
 
+const SAVE_PATH = "user://save.cfg"
+const DEBUG_SAVE_PATH = "res://save.cfg"
+
 @export var game_stats: GameStats
 
 @onready var score_value_label: Label      = %ScoreValueLabel
 @onready var high_score_value_label: Label = %HighScoreValueLabel
 
+var save_path = SAVE_PATH
+var high_score_key := "highscore"
 
 func _ready() -> void:
+    load_high_score()
     if game_stats.score > game_stats.high_score:
         game_stats.high_score = game_stats.score
+        save_high_score()
         
     score_value_label.text = str(game_stats.score)
     high_score_value_label.text = str(game_stats.high_score)
@@ -19,3 +26,17 @@ func _process(delta: float) -> void:
         game_stats.score = 0
         get_tree().change_scene_to_file("res://ui/menu.tscn")
 
+
+func load_high_score() -> void:
+    var config = ConfigFile.new()
+    var error = config.load(save_path)  
+    if error != OK: return
+    
+    game_stats.high_score = config.get_value("game", high_score_key)
+    
+
+func save_high_score() -> void:
+    var config = ConfigFile.new()
+    config.set_value("game", high_score_key, game_stats.high_score)
+    config.save(save_path)
+    
