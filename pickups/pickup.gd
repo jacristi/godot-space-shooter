0eea3993f6355a5c8f18f34ac240e6b9f8a12515
jacristi <jacristi0428@gmail.com>
2024-05-31 00:09:35 -0400

    feat: new pickup item

---
+++ pickups/pickup.gd
@@ -0,0 +1,34 @@
+extends Node2D
+
+
+@onready var move_component:  MoveComponent  = $MoveComponent
+@onready var stats_component: StatsComponent = $StatsComponent
+@onready var scale_component: ScaleComponent = $ScaleComponent
+@onready var flash_component: FlashComponent = $FlashComponent
+@onready var shake_component: ShakeComponent = $ShakeComponent
+@onready var score_component: ScoreComponent = $ScoreComponent
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+@onready var hitbox_component:  HitboxComponent  = $HitboxComponent
+@onready var destroyed_component: DestroyedComponent = $DestroyedComponent
+@onready var audio_player: VariablePitchAudioStreamPlayer = $VariablePitchAudioStreamPlayer
+
+
+func _ready() -> void:
+    stats_component.no_health.connect(give_score_on_death)
+    visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+    hurtbox_component.hurt.connect(was_hurt)
+    hitbox_component.hit_hurtbox.connect(give_score_on_collect)
+
+func was_hurt(hitbox: HitboxComponent) -> void:
+    flash_component.flash()
+    scale_component.tween_scale()
+    shake_component.tween_shake()
+    audio_player.play_with_variance()
+    
+func give_score_on_death() -> void:
+    score_component.adjust_score()
+
+func give_score_on_collect(hurtbox: HurtboxComponent) -> void:
+    score_component.adjust_score(100)
+    destroyed_component.destroy()
