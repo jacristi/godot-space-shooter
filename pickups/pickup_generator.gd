3c59d36faf59b7e60986539885be47c9ab992ca0
jacristi <jacristi0428@gmail.com>
2024-05-31 23:33:04 -0400

    feat : pickup spawner

--- pickups/pickup_generator.gd
+++ pickups/pickup_generator.gd
@@ -1,12 +1,38 @@
 extends Node2D
 
-@export var BluePickup: PackedScene
+@export var BluePickupScene: PackedScene
 
-# Called when the node enters the scene tree for the first time.
+var margin := 8
+var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+@export var game_stats: GameStats
+
+@onready var spawner_component: SpawnerComponent = $SpawnerComponent
+@onready var blue_pickup_spawn_timer: Timer = $BluePickupSpawnTimer
+
+
 func _ready() -> void:
-    pass # Replace with function body.
+    blue_pickup_spawn_timer.timeout.connect(
+        handle_pickup_spawn.bind(
+            BluePickupScene,
+            blue_pickup_spawn_timer
+        )
+    )
+
+    game_stats.score_changed.connect(check_enable_pickup_timers)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
+func check_enable_pickup_timers(new_score: int):
+    if new_score > 100:
+        blue_pickup_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+
+
+func handle_pickup_spawn(pickup_scene: PackedScene, timer: Timer) -> void:
+    spawner_component.scene = pickup_scene
+    spawner_component.spawn(
+        Vector2(
+            randf_range(margin, screen_width-margin),
+            5
+            )
+        )
+    timer.start()
