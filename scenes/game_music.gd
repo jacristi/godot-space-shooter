c69f7d7919aa33b147257fda4840df2bc87c0f8b
jacristi <jacristi0428@gmail.com>
2024-06-06 00:05:50 -0400

    feat: mute/unmute audio

--- scenes/game_music.gd
+++ scenes/game_music.gd
@@ -1,5 +1,21 @@
 extends AudioStreamPlayer
 
 
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus   = AudioServer.get_bus_index("SFX")
+
 func _ready() -> void:
     pass
+
+func _process(_delta: float) -> void:
+    if Input.is_action_just_pressed("mute_music"):
+        AudioServer.set_bus_mute(
+            music_bus,
+            not AudioServer.is_bus_mute(music_bus)
+            )
+
+    if Input.is_action_just_pressed("mute_sound"):
+        AudioServer.set_bus_mute(
+            sfx_bus,
+            not AudioServer.is_bus_mute(sfx_bus)
+            )
